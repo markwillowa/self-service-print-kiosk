de2ff23376c354e10e2a58cdc1819d227b1b7978
Mark Willow Aldave <mark.willow@orbio.world>
2026-05-14 11:18:45 +0800

    Added imports to web.php

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\RegistrationController;
 use App\Models\PrintJob;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
