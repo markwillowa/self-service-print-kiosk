ef4c03213dc274c0b0742dbb17a7f2bb0d01442f
Mark Willow Aldave <mark.willow@orbio.world>
2026-05-16 03:57:46 +0800

    added coin scanner 6

--- resources/coin/coin_scanner.py
+++ resources/coin/coin_scanner.py
@@ -1,29 +1,49 @@
 import RPi.GPIO as GPIO
 import time
-
-PIN = 19
+from datetime import datetime
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(
-    PIN,
-    GPIO.IN,
-    pull_up_down=GPIO.PUD_UP
-)
+IGNORE_PINS = {
+    0, 1, 2, 3, 4,
+    14, 15,
+}
 
-print(f"Watching GPIO{PIN}...")
+PINS = []
+
+for pin in range(2, 28):
+    if pin not in IGNORE_PINS:
+        PINS.append(pin)
+
+for pin in PINS:
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+last_state = {
+    pin: GPIO.input(pin)
+    for pin in PINS
+}
+
+print("Scanning ALL GPIO pins...")
+print("Insert coins now.")
 print("Press CTRL+C to stop.\n")
 
 try:
     while True:
-        state = GPIO.input(PIN)
+        for pin in PINS:
+            current_state = GPIO.input(pin)
 
-        print(
-            f"GPIO{PIN}: {'HIGH' if state else 'LOW'}",
-            end='\r'
-        )
+            if (
+                last_state[pin] == GPIO.HIGH and
+                current_state == GPIO.LOW
+            ):
+                print(
+                    f"[{datetime.now().strftime('%H:%M:%S.%f')[:-3]}] "
+                    f"PULSE DETECTED ON GPIO{pin}"
+                )
 
-        time.sleep(0.05)
+            last_state[pin] = current_state
+
+        time.sleep(0.001)
 
 except KeyboardInterrupt:
     print("\nStopped.")
